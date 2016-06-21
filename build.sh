c138fd54a65c4db6670c0a4639fac2cba5921474
haiviet2011 <haiviet2011@gmail.com>
2016-06-21 11:08:09 +0700

    ARM: add support for kernel mode NEON

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export ARCH=arm
 export CROSS_COMPILE=/home/haiviet/gcc/bin/arm-eabi-
-make ef47s_user_defconfig
-make -j2
+
+mkdir -p ./obj/KERNEL_OBJ/
+make ARCH=arm O=./obj/KERNEL_OBJ/ msm8960_ef45k_tp20_user_defconfig
+make -j2 ARCH=arm O=./obj/KERNEL_OBJ/
+cp -f ./obj/KERNEL_OBJ/arch/arm/boot/zImage .
