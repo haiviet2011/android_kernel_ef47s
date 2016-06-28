4660f14ec2caca5b6163b38edee62385ead7ecc3
haiviet2011 <haiviet2011@gmail.com>
2016-06-28 17:21:20 +0700

    Update defconfig

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export ARCH=arm
 export CROSS_COMPILE=/home/haiviet/gcc/bin/arm-eabi-
 
 mkdir -p ./obj/KERNEL_OBJ/
-make ARCH=arm O=./obj/KERNEL_OBJ/ msm8960_ef45k_tp20_user_defconfig
+make ARCH=arm O=./obj/KERNEL_OBJ/ ef47s_user_defconfig
 make -j2 ARCH=arm O=./obj/KERNEL_OBJ/
 cp -f ./obj/KERNEL_OBJ/arch/arm/boot/zImage .
